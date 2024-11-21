92f06db7139a4aed0275cdf93ea82d59c5103cac
Piotr Matejek <devmatejekpior@gmail.com>
2024-11-21 11:20:10 +0100

    added interaction for Form

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,10 +1,41 @@
+import { useState } from "react";
 import "./style.css";
+import { createId } from "../utilis/createId";
 
-export const Form = () => {
+export const Form = ({ tasks, addTask }) => {
+  const [formValue, setFormValue] = useState({});
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    
+    setFormValue((prevValues) => ({
+      ...prevValues,
+      [name]: value.trim(),
+    }));
+  };
+
+  const handleSubmitNewTask = (event) => {
+    event.preventDefault();
+
+    if (!formValue.content) return;
+
+    const newTask = {
+      id: createId(tasks),
+      content: formValue.content,
+      done: false,
+    };
+
+    addTask(newTask);
+    setFormValue({});
+  };
+
   return (
     <>
-      <form className="form">
+      <form onClick={handleSubmitNewTask} className="form">
         <input
+          name="content"
+          value={formValue.content || ""}
+          onChange={handleInputChange}
           className="form__input js-newTask"
           placeholder="Co jest do zrobienia?"
         />
