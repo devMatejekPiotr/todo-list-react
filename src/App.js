12f266709181a5f267b40128f7aaee2b8e091544
Piotr Matejek <devmatejekpior@gmail.com>
2024-11-21 03:01:54 +0100

    added interaction with isTaskDone

--- src/App.js
+++ src/App.js
@@ -3,22 +3,35 @@ import { Form } from "./Form";
 import { Header } from "./Header";
 import { Tasks } from "./Tasks";
 import { Section } from "./Section";
+import { useState } from "react";
 
 const tasks = [
   { content: "kupic bulki", done: false },
   { content: "kupic piwko", done: true },
 ];
 
-let isTaskDone = false;
+// let isTaskDone = false;
 
 function App() {
+  const [isTaskDone, setIsTaskDone] = useState(false);
+
+  const handleIsTaskDone = () => {
+    setIsTaskDone((prevState) => !prevState);
+  };
+
   return (
     <>
       <Header title="Lista zadań" />
       <Section title="Dodaj nowe zadanie" body={<Form />} />
       <Section
         title="Lista zadań"
-        body={<Buttons tasks={tasks} isTaskDone={isTaskDone} />}
+        body={
+          <Buttons
+            tasks={tasks}
+            isTaskDone={isTaskDone}
+            handleIsTaskDone={handleIsTaskDone}
+          />
+        }
       />
       <Tasks tasks={tasks} isTaskDone={isTaskDone} />
     </>
--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -1,14 +1,15 @@
 // import React from "react";
 import "./style.css";
 
-export const Buttons = ({ tasks, isTaskDone }) => {
+export const Buttons = ({ tasks, isTaskDone, handleIsTaskDone }) => {
+  console.log(handleIsTaskDone);
   const isAllTasksDone = tasks.every((task) => task.done === true);
   const disabledClass = isAllTasksDone ? ".button__item--disabled" : "";
 
   return (
     tasks.length > 0 && (
       <div className="buttons">
-        <button className="button__item">
+        <button onClick={handleIsTaskDone} className="button__item">
           {!isTaskDone ? "ukryj ukończone" : "pokaż ukończone"}
         </button>
         <button
